52b34c14b279c44e30e18401f4b20f71aed0e0e0
johnsuhr <john.conrad.suhr@gmail.com>
2013-11-14 12:34:09 -0800

    Create get_lobby_user_ct.php

---
+++ PHP Scripts/get_lobby_user_ct.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * The following code will retrieve the number of
+ * users in the lobby specified in lobbyId
+ * from the database and return the list in a JSON
+ */
+
+// connect to the database
+require("db_connect.php");
+
+// array for JSON response and set success to FALSE
+$response = array();
+$response["success"] = 0;
+$rowCount = 0;
+
+// check for lobbyId and store
+if (!empty($_POST))
+{
+        $lobbyId = $_POST["lobbyId"];
+}
+
+// Count users in lobby
+try
+{        
+        $stmt = $db->prepare("SELECT * FROM lobby_users WHERE lobby_id = ?");
+        $stmt->bind_param("s", $lobbyId);
+        $stmt->execute();
+        while ($stmt->fetch())
+        {        
+                $rowCount += 1;
+        }
+        $stmt->close();
+}
+catch (mysqli_exception $ex)
+{
+        $response["success"] = 0;
+        $response["message"] = "Database Error1. Please Try Again!";
+        die(json_encode($response));
+}
+
+$response["success"] = 1;
+$response["message"] = "Retrieve User Count Successful!";
+$response["numUsers"] = $rowCount;
+echo json_encode($response);
+
+?>
